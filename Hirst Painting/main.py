e7e1d412609ece04813347213518ccd9d5214346
GlitterBird <niyamplus@gmail.com>
2024-08-02 12:41:36 -0700

    Commit changes

---
+++ Hirst Painting/main.py
@@ -0,0 +1,32 @@
+import turtle
+from turtle import Turtle, Screen
+from random import choice
+
+# code used to extract colors from image
+# import colorgram
+# rgb_colors = []
+# colors = colorgram.extract('image.jpg', 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+# print(rgb_colors)
+
+turtle.colormode(255)
+color_list = [(249, 228, 17), (213, 13, 9), (198, 12, 35), (231, 228, 5), (197, 69, 20), (33, 90, 188), (43, 212, 71), (234, 148, 40), (33, 30, 152), (16, 22, 55), (66, 9, 49), (240, 245, 251), (244, 39, 149), (65, 202, 229), (14, 205, 222), (63, 21, 10), (224, 19, 111), (229, 165, 8), (15, 154, 22), (245, 58, 16), (98, 75, 9), (248, 11, 9), (222, 140, 203), (68, 240, 161), (10, 97, 62), (5, 38, 33), (68, 219, 155)]
+brush = Turtle()
+brush.hideturtle()
+brush.speed("fastest")
+brush.penup()
+brush.setposition(-200, -200)
+for i in range(10):
+    for x in range(10):
+        brush.dot(20, choice(color_list))
+        brush.forward(40)
+    position = list(brush.position())
+    brush.setposition(-200, position[1] + 40)
+
+screen = Screen()
+screen.exitonclick()
